e64b2cb1d0324b2cc7e7c5bd4849f8e811d62973
vamshi1159 <evk1027917@gmail.com>
2022-06-28 13:44:51 +0530

    converted to JPA

--- src/main/java/com/springboot/blinkist/dao/BookDAOImpl.java
+++ src/main/java/com/springboot/blinkist/dao/BookDAOImpl.java
@@ -1,13 +1,13 @@
 package com.springboot.blinkist.dao;
 
 import com.springboot.blinkist.entity.Book;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
@@ -17,38 +17,25 @@ public class BookDAOImpl implements BookDAO{
     private EntityManager entityManager;
 
     @Override
-    @Transactional
     public List<Book> findAll() {
-        //get session
-        Session currentSession = entityManager.unwrap(Session.class);
-
-        Query<Book> query = currentSession.createQuery("from Book",Book.class);
+        Query query = entityManager.createQuery("from Book");
         return query.getResultList();
     }
 
     @Override
-    @Transactional
     public Book findById(int id) {
-        //get session
-        Session currentSession = entityManager.unwrap(Session.class);
-
-        return currentSession.get(Book.class,id);
+        return entityManager.find(Book.class,id);
     }
 
     @Override
-    @Transactional
     public void save(Book book) {
-        //get session
-        Session currentSession = entityManager.unwrap(Session.class);
-        currentSession.saveOrUpdate(book);
+        Book book1=entityManager.merge(book);
+        book.setId(book1.getId());
     }
 
     @Override
-    @Transactional
     public void deleteById(int id) {
-        //get session
-        Session currentSession = entityManager.unwrap(Session.class);
-        Query query = currentSession.createQuery("delete from Book where id=:id");
+        Query query = entityManager.createQuery("delete from Book where id=:id");
         query.setParameter("id",id);
         query.executeUpdate();
     }
